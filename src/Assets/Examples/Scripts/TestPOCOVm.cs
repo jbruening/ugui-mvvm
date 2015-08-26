d93f376b3f6c54261416dbe8e3634953a0f74e65
Justin <robojoos@gmail.com>
2015-08-26 16:25:03 -0700

    basic poco use scene, renamed 'scene' scene to demo.

---
+++ src/Assets/Examples/Scripts/TestPOCOVm.cs
@@ -0,0 +1,83 @@
+﻿using uguimvvm;
+using UnityEngine;
+using System.Collections;
+
+class TestPOCOVm : AViewModel
+{
+    string _testString;
+    public string TestString
+    {
+        get { return _testString; }
+        set { SetProperty("TestString", ref _testString, value); }
+    }
+
+    private bool _testBool;
+    public bool TestBool
+    {
+        get { return _testBool; }
+        set { SetProperty("TestBool", ref _testBool, value); }
+    }
+
+    private bool _canModifyChildren;
+    public bool CanModifyChildren
+    {
+        get { return _canModifyChildren; }
+        set
+        {
+            if (SetProperty("CanModifyChildren", ref _canModifyChildren, value))
+            {
+                AddChild.RaiseCanExecuteChanged();
+                RemoveChild.RaiseCanExecuteChanged();
+            }
+        }
+    }
+
+    private bool _canSaySomething;
+    public bool CanSaySomething
+    {
+        get { return _canSaySomething; }
+        set
+        {
+            if (SetProperty("CanSaySomething", ref _canSaySomething, value))
+                SaySomething.RaiseCanExecuteChanged();
+        }
+    }
+
+    public ObservableCollection<ChildViewModel> Children { get; private set; }
+
+    public TestPOCOVm()
+    {
+        Children = new ObservableCollection<ChildViewModel>();
+        
+        AddChild = new RelayCommand(ImplAddChild, ImplCanModifyChildren);
+        RemoveChild = new RelayCommand(ImplRemoveChild, ImplCanModifyChildren);
+        SaySomething = new RelayCommand(ImplSaySomething, ImplCanSaySomething);
+    }
+
+    public RelayCommand SaySomething { get; private set; }
+    public RelayCommand AddChild { get; private set; }
+    public RelayCommand RemoveChild { get; private set; }
+
+    bool ImplCanModifyChildren()
+    {
+        return CanModifyChildren;
+    }
+    void ImplAddChild()
+    {
+        Children.Add(new ChildViewModel());
+    }
+    void ImplRemoveChild()
+    {
+        if (Children.Count == 0) return;
+        Children.RemoveAt(Children.Count - 1);
+    }
+
+    void ImplSaySomething()
+    {
+        TestString = "Something!";
+    }
+    bool ImplCanSaySomething()
+    {
+        return CanSaySomething;
+    }
+}
