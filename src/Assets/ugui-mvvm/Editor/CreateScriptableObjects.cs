551ffb287dadcd0091b6e760a196dfca87adf5f4
Justin <robojoos@gmail.com>
2015-09-03 12:27:48 -0700

    add scriptable object wizard
    editor wizard to create scriptable objects of anything that inherits from scriptableobject.

---
+++ src/Assets/ugui-mvvm/Editor/CreateScriptableObjects.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+class CreateScriptableObjects : ScriptableWizard
+{
+    private static Type[] _scriptTypes;
+    private static string[] _names;
+    private int _idx;
+
+    [MenuItem("Assets/Create/Scriptable object")]
+    public static void CreateWizard()
+    {
+        if (_scriptTypes == null)
+        {
+            var ueda = typeof(ScriptableWizard).Assembly;
+            var editorTypes = ueda.GetTypes().Where(t => typeof(ScriptableObject).IsAssignableFrom(t));
+
+            var validAssemblies = AppDomain.CurrentDomain.GetAssemblies().Where(a => !Equals(a, ueda) && !a.FullName.Contains("UnityEditor"));
+            //all types deriving from ScriptableObject, that aren't editor-based classes.
+            _scriptTypes = validAssemblies
+                    .SelectMany(a => a.GetTypes())
+                    .Where(t => typeof(ScriptableObject).IsAssignableFrom(t) && !editorTypes.Any(e => e.IsAssignableFrom(t)))
+                    .ToArray();
+            _names = _scriptTypes.Select(t => t.FullName).ToArray();
+        }
+
+        var wizard = DisplayWizard<CreateScriptableObjects>("Create scriptable object");
+        wizard._idx = 0;
+    }
+
+    protected override bool DrawWizardGUI()
+    {
+        var idx = EditorGUILayout.Popup(_idx, _names);
+        if (idx == _idx) return false;
+        _idx = idx;
+        return true;
+    }
+
+    void OnWizardCreate()
+    {
+        var type = _scriptTypes[_idx];
+        var asset = ScriptableObject.CreateInstance(type);
+        if (asset == null)
+        {
+            EditorUtility.DisplayDialog("Error", string.Format("Cannot create an asset for type {0}", type), "Okay");
+            return;
+        }
+        ProjectWindowUtil.CreateAsset(asset, "New " + type.Name + ".asset");
+    }
+}
