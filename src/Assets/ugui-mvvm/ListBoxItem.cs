c0d8eb6f499b0f341fbbea218f6c58c8828cebf1
Justin <robojoos@gmail.com>
2016-02-09 09:48:22 -0800

    forgot some files from adding the listbox

---
+++ src/Assets/ugui-mvvm/ListBoxItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.UI;
+
+namespace uguimvvm
+{
+    class ListBoxItem : Selectable
+    {
+        public bool IsSelected()
+        {
+            return currentSelectionState == SelectionState.Highlighted || currentSelectionState == SelectionState.Pressed;
+        }
+    }
+}
