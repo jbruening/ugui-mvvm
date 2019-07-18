cdde728de64b0e315df7fbf2417fbf51e57c90ab
Adam Mitchell <adammit@microsoft.com>
2019-07-18 09:58:10 -0700

    clean up and get it working better

--- src/Assets/ugui-mvvm/Editor/ComponentReferenceDrawer.cs
+++ src/Assets/ugui-mvvm/Editor/ComponentReferenceDrawer.cs
@@ -30,104 +30,74 @@ class ComponentReferenceDrawer : PropertyDrawer
 
     public static void PropertyField(Rect position, SerializedProperty property)
     {
-        const float dropDownWidth = 20.0f;
+        float dropDownWidth = (position.width - EditorGUIUtility.labelWidth) / 3.0f;
 
         position.width -= dropDownWidth;
         EditorGUI.PropertyField(position, property);
 
         var dropdownPosition = new Rect(position.xMax, position.y, dropDownWidth, EditorGUIUtility.singleLineHeight);
 
-        //if (EditorGUI.DropdownButton(dropdownPosition, new GUIContent(), FocusType.Keyboard))
-        {
-            //GenericMenu dropdownMenu = new GenericMenu();
-            var dropdownItems = new List<DropdownItem>();
-            int selectedItem = -1;
+        var dropdownItems = new List<DropdownItem>();
+        int selectedItem = -1;
 
-            if (clipboard != null)
+        if (clipboard != null)
+        {
+            dropdownItems.Add(new DropdownItem
             {
-                //dropdownMenu.AddItem(
-                //    new GUIContent($"Paste reference to {clipboard.name} - {clipboard.GetType()}"),
-                //    false,
-                //    () => { property.objectReferenceValue = clipboard; });
+                Label = $"Paste reference to {clipboard.name} - {clipboard.GetType()}",
+                Command = () => { property.objectReferenceValue = clipboard; },
+            });
+        }
+        else
+        {
+            dropdownItems.Add(new DropdownItem { Label = "Nothing to paste" });
+        }
+
+        var component = property.objectReferenceValue as Component;
+        if (component != null && component.gameObject != null)
+        {
+            dropdownItems.Add(new DropdownItem { Label = null });
+
+            var siblingComponents = component.gameObject.GetComponents<Component>();
+
+            foreach (var siblingComponent in siblingComponents)
+            {
+                bool currentlySelected = siblingComponent == component;
+
+                if (currentlySelected)
+                {
+                    // This component is currently selected.  Mark it as such.
+                    selectedItem = dropdownItems.Count;
+                }
+
                 dropdownItems.Add(new DropdownItem
                 {
-                    Label = $"Paste reference to {clipboard.name} - {clipboard.GetType()}",
-                    Command = () => { property.objectReferenceValue = clipboard; },
+                    Label = $"{siblingComponent.GetType().Name}",
+                    Command = () => { property.objectReferenceValue = siblingComponent; },
                 });
+            }
+        }
+
+        var dropdownContent = new GUIContent[dropdownItems.Count];
+        for (int i = 0; i < dropdownContent.Length; i++)
+        {
+            var item = dropdownItems[i];
+
+            if (item.Label == null)
+            {
+                dropdownContent[i] = new GUIContent();
             }
             else
             {
-                //dropdownMenu.AddDisabledItem(new GUIContent("No component reference to paste."));
-                dropdownItems.Add(new DropdownItem { Label = "Nothing to paste.  Right click on a component and select \"Copy Component Reference\"" });
+                dropdownContent[i] = new GUIContent(item.Label);
             }
-
-            var component = property.objectReferenceValue as Component;
-            if (component != null && component.gameObject != null)
-            {
-                dropdownItems.Add(new DropdownItem { Label = null });
-                //dropdownItems.Add(new DropdownItem { Label = "-------------------" });
-                //dropdownMenu.AddSeparator("");
-
-                var siblingComponents = component.gameObject.GetComponents<Component>();
-
-                foreach (var siblingComponent in siblingComponents)
-                {
-                    bool currentlySelected = siblingComponent == component;
-
-                    //dropdownMenu.AddItem(
-                    //    new GUIContent($"{siblingComponent.name} - {siblingComponent.GetType()}"),
-                    //    currentlySelected,
-                    //    () => {
-                    //        property.objectReferenceValue = siblingComponent;
-                    //        Debug.Log($"select - {siblingComponent.name} - {siblingComponent.GetType()}");
-                    //    });
-
-                    if (currentlySelected)
-                    {
-                        // This component is currently selected.  Mark it as such.
-                        selectedItem = dropdownItems.Count;
-                    }
-
-                    dropdownItems.Add(new DropdownItem
-                    {
-                        Label = $"Paste reference to {siblingComponent.name} - {siblingComponent.GetType()}",
-                        Command = () => { property.objectReferenceValue = siblingComponent; },
-                    });
-                }
-            }
-
-            var dropdownContent = new GUIContent[dropdownItems.Count];
-            for (int i = 0; i < dropdownContent.Length; i++)
-            {
-                var item = dropdownItems[i];
-
-                if (item.Label == null)
-                {
-                    dropdownContent[i] = new GUIContent();
-                }
-                else
-                {
-                    dropdownContent[i] = new GUIContent(item.Label);
-                }
-            }
-
-            var clickedIndex = EditorGUI.Popup(dropdownPosition, selectedItem, dropdownContent);
-
-            if (clickedIndex != selectedItem)
-            {
-                dropdownItems[clickedIndex].Command?.Invoke();
-            }
-
-            //Debug.Log("before dropdown");
-            //dropdownMenu.DropDown(dropdownPosition);
-            //Debug.Log("after dropdown");
         }
 
-        //Debug.Log($"{property.propertyPath} - {property.serializedObject} - {property.exposedReferenceValue} - {property.objectReferenceInstanceIDValue} - {property.objectReferenceValue}");
+        var clickedIndex = EditorGUI.Popup(dropdownPosition, selectedItem, dropdownContent);
 
-        //if (clipboard != null && GUI.Button(new Rect(position.xMax, position.y, 10, 10), new GUIContent("+", "paste reference to " + clipboard.name + "-" + clipboard.GetType())))
-        //{
-        //    property.objectReferenceValue = clipboard;
-        //}
+        if (clickedIndex != selectedItem)
+        {
+            dropdownItems[clickedIndex].Command?.Invoke();
+        }
     }
 }
