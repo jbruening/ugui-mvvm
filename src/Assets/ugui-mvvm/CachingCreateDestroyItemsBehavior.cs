4ab32f5cd16ddba7290117776f23c738e5e5516f
Justin Savino <jusavino@microsoft.com>
2021-06-17 15:43:45 -0700

    - refactor out caching from itemsControl into an implemented interface behavior - DefaulteCreateDestroyItemsBehavior contains default instantiating and destroying behavior from old ItemsControl

---
+++ src/Assets/ugui-mvvm/CachingCreateDestroyItemsBehavior.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace uguimvvm
+{
+    /// <summary>
+    /// Caching behavior creates a pool of hidden GameObjects when it is constructed, then activates
+    /// and deactives them as they are assigned to items. If more items are needed than the initial amount
+    /// they will default to being instatiated and added GameObject pool. 
+    /// </summary>
+    public class CachingCreateDestroyItemsBehavior : ICreateDestroyItems
+    {
+        /// <summary>
+        /// The collection of the item's GameObjects generated for caching 
+        /// </summary>
+        private readonly List<GameObject> _gameObjectsCached = new List<GameObject>();
+
+        /// <summary>
+        /// Caching Behavior looks to other behaviors for instantiating and handling templates
+        /// </summary>
+        private readonly ICreateDestroyItems _createDestroyImpl;
+
+        /// <summary>
+        /// Correlating items to their cached GameObjects  
+        /// </summary>
+        private readonly Dictionary<object, GameObject> _itemToGameObjectMap = new Dictionary<object, GameObject>();
+
+        public CachingCreateDestroyItemsBehavior(ICreateDestroyItems createDestroyInfo, int initialPoolSize)
+        {
+            _createDestroyImpl = createDestroyInfo;
+
+            // Instantiate the starting pool of GameObjects
+            for (var i = 0; i < initialPoolSize; i++)
+            {
+                var newGameObject = InstantiateItem();
+                // These item GameObjects haven't been asigned yet so hide them
+                newGameObject.SetActive(false);
+                _gameObjectsCached.Add(newGameObject);
+            }  
+        }
+
+        public GameObject CreateItemControl(object item)
+        {
+            GameObject control = GetCachedControl();
+ 
+            if (control != null)
+            {
+                // Unused controls were set inactive
+                control.SetActive(true);
+            }
+            else
+            {
+                // There wasn't any in the pool left, create a new one and add it to the pool
+                control = InstantiateItem();
+                _gameObjectsCached.Add(control);
+            }
+
+            _itemToGameObjectMap.Add(item, control);
+
+            return control;
+        }
+
+        public void DestroyItemControl(ItemsControl.ItemInfo item)
+        {
+            // Hide item
+            item.Control.SetActive(false);
+            // Free up gameobject from the pool so it can be reasigned
+            _itemToGameObjectMap.Remove(item.Item);
+        }
+
+        private GameObject GetCachedControl()
+        {
+            GameObject control = null;
+
+            // Find a control that isn't already mapped to an item 
+            foreach (var go in _gameObjectsCached)
+            {
+                if (!_itemToGameObjectMap.ContainsValue(go))
+                {
+                    control = go;
+                    break;
+                }
+            }
+
+            return control;
+        }
+
+        public GameObject InstantiateItem()
+        {
+            return(_createDestroyImpl.InstantiateItem());
+        }
+
+        public void SetItemTemplate(GameObject itemTemplate)
+        {
+            _createDestroyImpl.SetItemTemplate(itemTemplate);
+        }
+
+        public void SetItemTemplate(DataTemplateSelector dataTemplateSelector)
+        {
+            _createDestroyImpl.SetItemTemplate(dataTemplateSelector);
+        }
+    }
+}
---
+++ src/Assets/ugui-mvvm/DefaultCreateDestroyItemsBehavior.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+namespace uguimvvm
+{
+    /// <summary>
+    /// Defualt Behavior for items is to Instantiate and Destroy the GameObjects associated to the Items
+    /// as needed. 
+    /// </summary>
+    public class DefaultCreateDestroyItemsBehavior : ICreateDestroyItems
+    {
+        private GameObject _itemTemplate;
+
+        private DataTemplateSelector _itemTemplateSelector;
+
+        private Transform _itemParent;
+
+        public DefaultCreateDestroyItemsBehavior(Transform parent, GameObject itemTemplate, DataTemplateSelector itemTemplateSelector)
+        {
+            _itemParent = parent;
+            _itemTemplate = itemTemplate;
+            _itemTemplateSelector = itemTemplateSelector;
+        }
+
+        public GameObject CreateItemControl(object item)
+        {
+            CheckItemTemplateSelector(item);
+
+            return InstantiateItem();
+        }
+
+        public void DestroyItemControl(ItemsControl.ItemInfo item)
+        {
+            var rect = item.Control.GetComponent<RectTransform>();
+            if (rect == null)
+                item.Control.transform.parent = null;
+            else
+                rect.SetParent(null, false);
+
+            Object.Destroy(item.Control);
+        }
+
+        public GameObject InstantiateItem()
+        {
+            if (_itemTemplate == null)
+            {
+                throw new System.NotImplementedException("There is no ItemTemplate set for this ItemsControl list");
+            }
+
+            var newGameObject = Object.Instantiate(_itemTemplate);
+            var rect = newGameObject.GetComponent<RectTransform>();
+            if (rect == null)
+                newGameObject.transform.parent = _itemParent;
+            else
+                rect.SetParent(_itemParent, false);
+
+            return newGameObject;
+        }
+
+        public void CheckItemTemplateSelector(object item)
+        {
+            // MRMW Start Change - Prefab Selector support
+            _itemTemplate = _itemTemplateSelector != null ? _itemTemplateSelector.SelectTemplate(item) : _itemTemplate;
+            // MRMW End Change - Prefab Selector support
+        }
+
+        public void SetItemTemplate(GameObject itemTemplate)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetItemTemplate(DataTemplateSelector dataTemplateSelector)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
